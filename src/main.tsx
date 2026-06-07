feaf1b64618b4b6c41b7fadc1f41429c8d2be023
Nutree AI <nutreeaidev@gmail.com>
2026-06-07 20:02:35 +0700

    feat: swap MockApi for FirebaseApi in provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,11 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { App } from "./App";
 import { ApiContext } from "./api";
-import { createMockApi } from "./api/mockApi";
+import { createFirebaseApi } from "./api/firebaseApi";
 import { ThemeProvider } from "./lib/ThemeContext";
 import "./index.css";
 
-const api = createMockApi();
+const api = createFirebaseApi();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
